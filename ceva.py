b07ca7d1d46b58b03cd1457e00fc7da94e05b0f5
Crisian Flaviu <flaviu_remus@yahoo.com>
2021-04-24 18:44:58 +0100

    partial commit

--- ceva.py
+++ ceva.py
@@ -9,6 +9,7 @@ import cv2
 from imutils.video import VideoStream
 import pika
 
+
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host="roedeer-01.rmq.cloudamqp.com",
                               virtual_host="tkfdwjro",
@@ -26,12 +27,9 @@ ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
 args = vars(ap.parse_args())
 
 print("[INFO] starting video stream...")
-# vs = VideoStream(src=0).start()
 vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
-# open the output CSV file for writing and initialize the set of
-# barcodes found thus far
-csv = open(args["output"], "w")
+
 found = set()
 
 # loop over the frames from the video stream
@@ -40,41 +38,45 @@ while True:
     # have a maximum width of 400 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
+
     # find the barcodes in the frame and decode each of the barcodes
     barcodes = pyzbar.decode(frame)
     # loop over the detected barcodes
     for barcode in barcodes:
-        print("barcode")
+
         # extract the bounding box location of the barcode and draw
         # the bounding box surrounding the barcode on the image
         (x, y, w, h) = barcode.rect
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
         # the barcode data is a bytes object so if we want to draw it
         # on our output image we need to convert it to a string first
+
         barcodeData = barcode.data.decode("utf-8")
         barcodeType = barcode.type
 
-        print(barcodeData)
-        channel.basic_publish(exchange='', routing_key='barcode_queue', body='Buna from Rasberry' + barcodeData)
+        print(datetime.datetime.now(),barcodeData )
+
+        time.sleep(5)
+        channel.basic_publish(exchange='', routing_key='barcode_queue', body=barcodeData)
 
         # draw the barcode data and barcode type on the image
-        text = "{} ({})".format(barcodeData, barcodeType)
-        cv2.putText(frame, text, (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        # text = "{} ({})".format(barcodeData, barcodeType)
+        #   cv2.putText(frame, text, (x, y - 10),
+        #               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         # if the barcode text is currently not in our CSV file, write
         # the timestamp + barcode to disk and update the set
-        if barcodeData not in found:
-            csv.write("{},{}\n".format(datetime.datetime.now(),
-                                       barcodeData))
-            csv.flush()
-            found.add(barcodeData)
+        # if barcodeData not in found:
+        #     csv.write("{},{}\n".format(datetime.datetime.now(),
+        #                                barcodeData))
+            # csv.flush()
+            # found.add(barcodeData)
         # show the output frame
-        cv2.imshow("Barcode Scanner", frame)
-        key = cv2.waitKey(1) & 0xFF
+        # cv2.imshow("Barcode Scanner", frame)
+        # key = cv2.waitKey(1) & 0xFF
 
         # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
+        #  if key == ord("q"):
+        #      break
 # close the output CSV file do a bit of cleanup
 print("[INFO] cleaning up...")
 csv.close()
