c4636668a719df326f32ce79ccd8c0ffd52e2d22
Crisian Flaviu <flaviu_remus@yahoo.com>
2021-06-07 16:14:13 +0100

    partial commit

--- ceva.py
+++ ceva.py
@@ -1,5 +1,4 @@
-# import the necessary packages
-from imutils.video import VideoStream
+
 from pyzbar import pyzbar
 import argparse
 import datetime
@@ -8,6 +7,13 @@ import time
 import cv2
 from imutils.video import VideoStream
 import pika
+import json
+from playsound import playsound
+import os
+import vlc
+
+p = vlc.MediaPlayer("/home/pi/Desktop/findBarcode/ceva.mp3")
+p.play()
 
 
 connection = pika.BlockingConnection(
@@ -20,65 +26,38 @@ channel = connection.channel()
 
 channel.queue_declare(queue='barcode_queue')
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
-                help="path to output CSV file containing barcodes")
-args = vars(ap.parse_args())
-
 print("[INFO] starting video stream...")
 vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
-found = set()
+userWebsocket = None
 
-# loop over the frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it to
-    # have a maximum width of 400 pixels
+
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
 
-    # find the barcodes in the frame and decode each of the barcodes
     barcodes = pyzbar.decode(frame)
-    # loop over the detected barcodes
     for barcode in barcodes:
-
-        # extract the bounding box location of the barcode and draw
-        # the bounding box surrounding the barcode on the image
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        # the barcode data is a bytes object so if we want to draw it
-        # on our output image we need to convert it to a string first
-
         barcodeData = barcode.data.decode("utf-8")
         barcodeType = barcode.type
 
-        print(datetime.datetime.now(),barcodeData )
+        if barcodeType == 'QRCODE':
+            decodedBarcode = json.loads(barcodeData)
+            print(decodedBarcode)
+            print(decodedBarcode['name'])
 
+
+            userWebsocket = decodedBarcode['id']
+
+        else:
+            print(datetime.datetime.now(), barcodeData, barcodeType)
+            if (userWebsocket is None):
+                print("please login before")
+            else:
+                sentObj = {"socket": userWebsocket, "barcode": barcodeData}
+                jsonSentObj = json.dumps(sentObj)
+                channel.basic_publish(exchange='', routing_key='barcode_queue', body=jsonSentObj)
         time.sleep(5)
-        channel.basic_publish(exchange='', routing_key='barcode_queue', body=barcodeData)
 
-        # draw the barcode data and barcode type on the image
-        # text = "{} ({})".format(barcodeData, barcodeType)
-        #   cv2.putText(frame, text, (x, y - 10),
-        #               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        # if the barcode text is currently not in our CSV file, write
-        # the timestamp + barcode to disk and update the set
-        # if barcodeData not in found:
-        #     csv.write("{},{}\n".format(datetime.datetime.now(),
-        #                                barcodeData))
-            # csv.flush()
-            # found.add(barcodeData)
-        # show the output frame
-        # cv2.imshow("Barcode Scanner", frame)
-        # key = cv2.waitKey(1) & 0xFF
-
-        # if the `q` key was pressed, break from the loop
-        #  if key == ord("q"):
-        #      break
-# close the output CSV file do a bit of cleanup
-print("[INFO] cleaning up...")
-csv.close()
-cv2.destroyAllWindows()
 vs.stop()
